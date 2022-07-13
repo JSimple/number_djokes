0e637480a395721be370e61121c2b729418e31f4
JSimple <josephinemayasimple@gmail.com>
2022-07-13 19:58:37 -0400

    made models better

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,31 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField('question text', max_length=200)
     pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        display_info = {
+            'id': self.id,
+            'text': self.question_text,
+            'pub date': self.pub_date
+        }
+        return display_info
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        display_info = {
+            'id': self.id,
+            'question': Question.objects.filter(id=self.question)[0].question_text,
+            'text': self.question_text,
+            'pub date': self.pub_date
+        }
+        return display_info
