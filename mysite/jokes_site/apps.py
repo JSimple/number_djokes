ce75409850e0e772c5c8cc79c9c87170e341530f
JSimple <josephinemayasimple@gmail.com>
2022-07-25 21:00:50 -0400

    add number_jokes submodule

--- mysite/jokes_site/apps.py
+++ mysite/jokes_site/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NumberjokesConfig(AppConfig):
+class Jokes_siteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'numberjokes'
+    name = 'jokes'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('numberjokes/', include('jokes_site.urls')),
     path('admin/', admin.site.urls),
 ]
