3020eaca3047543dbd4b763cc200b2bd3f97e4b0
JSimple <josephinemayasimple@gmail.com>
2022-07-28 13:12:24 -0400

    add: numberjokes details view and template

--- mysite/jokes_site/urls.py
+++ mysite/jokes_site/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:joke_id>/', views.detail, name='detail'),
 ]
--- mysite/jokes_site/views.py
+++ mysite/jokes_site/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import * 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the numberjokes index.")
+
+def detail(request, joke_id):
+    joke = get_object_or_404(Joke, pk=joke_id)
+    return render(request, 'numberjokes/detail.html', {'joke': joke})
