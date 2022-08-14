e31631f392c97f87523d783830d4b78f68cb28b4
JSimple <josephinemayasimple@gmail.com>
2022-08-14 18:15:21 -0400

    built static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,7 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'numberjokes_frontend/build')
+]
+
+# Media
+# MEDIA_URL = '/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
