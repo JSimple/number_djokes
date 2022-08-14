d9e687dd058252d5598d90c8a291d89819139152
JSimple <josephinemayasimple@gmail.com>
2022-08-14 18:37:57 -0400

    index file serving

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,8 +124,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'numberjokes_frontend/build')
 ]
 
+WHITENOISE_INDEX_FILE = True
+
 # Media
 MEDIA_URL = 'http://example.com'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
