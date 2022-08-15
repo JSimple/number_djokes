32ebfda980397a9a54644bfe4e5c211ab7fc965f
JSimple <josephinemayasimple@gmail.com>
2022-08-14 22:03:47 -0400

    trying to get prod / to serve index

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += [path(
-    '', serve, kwargs={ 'path': 'index.html'})]
+# if settings.DEBUG:
+urlpatterns += [path(
+'', serve, kwargs={ 'path': 'index.html'})]
